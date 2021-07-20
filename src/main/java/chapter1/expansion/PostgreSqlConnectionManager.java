5c3da446811ca469c4e6ca12908302d35c08c808
jon890 <sks070400>
2021-07-20 21:41:51 +0900

    1.3 DAO의 확장 공부 => 확장에는 열려있게 설계하는 방법 공부

---
+++ src/main/java/chapter1/expansion/PostgreSqlConnectionManager.java
@@ -0,0 +1,14 @@
+package chapter1.expansion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSqlConnectionManager implements ConnectionMaker{
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection("jdbc:postgresql://localhost/toby_spring", "postgres", "123456789");
+    }
+}
---
+++ src/main/java/chapter1/expansion/UserDaoTest.java
@@ -0,0 +1,32 @@
+package chapter1.expansion;
+
+import chapter1.model.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // UserDao가 사용할 ConnectionMaker 구현 클래스를 결정하고 객체를 만든다
+        ConnectionMaker connectionMaker = new PostgreSqlConnectionManager();
+        
+        // 1. UserDao 생성
+        // 2. 사용할 ConnectionMaker 타입의 객체 전달 => 결국 두 객체 사이의 의존관계 설정 효과
+        UserDao dao = new UserDao(connectionMaker);
+
+        User user = new User();
+        user.setId("bifos");
+        user.setName("김병태");
+        user.setPassword("12345678");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
