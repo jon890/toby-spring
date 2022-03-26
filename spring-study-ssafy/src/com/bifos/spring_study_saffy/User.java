d4c18967130e8b602b34c2ba8227162dffb86baf
BiFoS <jon89071@gmail.com>
2022-03-26 17:56:15 +0900

    1장. 초난감 DAO 작성

---
+++ spring-study-ssafy/src/com/bifos/spring_study_saffy/User.java
@@ -0,0 +1,13 @@
+package com.bifos.spring_study_saffy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+	
+	private String id;
+	private String name;
+	private String password;	
+}
